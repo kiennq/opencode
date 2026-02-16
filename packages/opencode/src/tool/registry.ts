df96003fdd3db779e983001cb0109278c8d201f5
Kien Nguyen <kien.n.quang@gmail.com>
2026-02-15 16:13:34 -0800

    feat(pwsh): add PowerShell Core tool with Windows security hardening

--- packages/opencode/src/tool/registry.ts
+++ packages/opencode/src/tool/registry.ts
@@ -1,7 +1,6 @@
 import { PlanExitTool } from "./plan"
 import { QuestionTool } from "./question"
 import { BashTool } from "./bash"
-import { PwshTool } from "./pwsh"
 import { EditTool } from "./edit"
 import { GlobTool } from "./glob"
 import { GrepTool } from "./grep"
@@ -24,7 +23,6 @@ import { Plugin } from "../plugin"
 import { WebSearchTool } from "./websearch"
 import { CodeSearchTool } from "./codesearch"
 import { Flag } from "@/flag/flag"
-import { Shell } from "@/shell/shell"
 import { Log } from "@/util/log"
 import { LspTool } from "./lsp"
 import { Truncate } from "./truncation"
@@ -106,7 +104,6 @@ export namespace ToolRegistry {
       InvalidTool,
       ...(question ? [QuestionTool] : []),
       BashTool,
-      ...(Shell.pwsh() ? [PwshTool] : []),
       ReadTool,
       GlobTool,
       GrepTool,
