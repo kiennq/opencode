36757eb194d047c72495f1fe940a5f1a56c1faa4
Kien Nguyen <kien.n.quang@gmail.com>
2026-02-25 14:33:56 -0800

    feat(timeline): add offset pagination and incremental loading

--- packages/sdk/js/src/v2/gen/types.gen.ts
+++ packages/sdk/js/src/v2/gen/types.gen.ts
@@ -3296,6 +3296,10 @@ export type SessionMessagesData = {
      * Maximum number of messages to return
      */
     limit?: number
+    /**
+     * Number of newest messages to skip
+     */
+    offset?: number
   }
   url: "/session/{sessionID}/message"
 }
